966f24772464373d8aa249d16548b0b9ac7c5617
yukijina <yukijina@gmail.com>
2020-10-03 21:15:49 -0700

    add more sections.split questions to a single file

--- src/components/Estimate.js
+++ src/components/Estimate.js
@@ -11,29 +11,16 @@ import IconButton from '@material-ui/core/IconButton';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import TextField from '@material-ui/core/TextField';
-import software from '../assets/software.svg';
-import mobile from '../assets/mobile.svg';
-import website from '../assets/website.svg';
 import backArrow from '../assets/backArrow.svg';
 import forwardArrow from '../assets/forwardArrow.svg';
 import backArrowDisabled from '../assets/backArrowDisabled.svg';
 import forwardArrowDisabled from '../assets/forwardArrowDisabled.svg';
-import camera from '../assets/camera.svg';
-import upload from '../assets/upload.svg';
-import person from '../assets/person.svg';
-import persons from '../assets/persons.svg';
-import people from '../assets/people.svg';
-import info from '../assets/info.svg';
-import bell from '../assets/bell.svg';
-import users from '../assets/users.svg';
-import iphone from '../assets/iphone.svg';
-import gps from '../assets/gps.svg';
-import customized from '../assets/customized.svg';
-import data from '../assets/data.svg';
-import android from '../assets/android.svg';
-import globe from '../assets/globe.svg';
-import biometrics from '../assets/biometrics.svg';
 import estimateAnimation from '../animations/estimateAnimation/data.json';
+import {
+  defaultQuestions,
+  softwareQuestions,
+  websiteQuestions,
+} from './estimateQuestion';
 
 const useStyles = makeStyles((theme) => ({
   icon: {
@@ -59,328 +46,6 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const defaultQuestions = [
-  {
-    id: 1,
-    title: 'Which service are you interested in?',
-    active: true,
-    options: [
-      {
-        id: 1,
-        title: 'Custom Software Development',
-        subtitle: null,
-        icon: software,
-        iconAlt: 'three floating screens',
-        selected: false,
-        cost: 0,
-      },
-      {
-        id: 2,
-        title: 'iOS/Android App Development',
-        subtitle: null,
-        icon: mobile,
-        iconAlt: 'outlines of phones and tablets',
-        selected: false,
-        cost: 0,
-      },
-      {
-        id: 3,
-        title: 'Website Development',
-        subtitle: null,
-        icon: website,
-        iconAlt: 'computer outline',
-        selected: false,
-        cost: 0,
-      },
-    ],
-  },
-];
-
-const softwareQuestions = [
-  { ...defaultQuestions[0], active: false },
-  // {
-  //   id: 1,
-  //   title: 'Which service are you interested in?',
-  //   active: false,
-  //   options: [
-  //     {
-  //       id: 1,
-  //       title: 'Custom Software Development',
-  //       subtitle: null,
-  //       icon: software,
-  //       iconAlt: 'three floating screens',
-  //       selected: false,
-  //       cost: 0,
-  //     },
-  //     {
-  //       id: 2,
-  //       title: 'iOS/Android App Development',
-  //       subtitle: null,
-  //       icon: mobile,
-  //       iconAlt: 'outlines of phones and tablets',
-  //       selected: false,
-  //       cost: 0,
-  //     },
-  //     {
-  //       id: 3,
-  //       title: 'Website Development',
-  //       subtitle: null,
-  //       icon: website,
-  //       iconAlt: 'computer outline',
-  //       selected: false,
-  //       cost: 0,
-  //     },
-  //   ],
-  // },
-  {
-    id: 2,
-    title: 'Which platforms do you need supported?',
-    subtitle: 'Select all that apply.',
-    options: [
-      {
-        id: 1,
-        title: 'Web Application',
-        subtitle: null,
-        icon: website,
-        iconAlt: 'computer outline',
-        selected: false,
-        cost: 1000,
-      },
-      {
-        id: 2,
-        title: 'iOS Application',
-        subtitle: null,
-        icon: iphone,
-        iconAlt: 'outline of iphone',
-        selected: false,
-        cost: 1000,
-      },
-      {
-        id: 3,
-        title: 'Android Application',
-        subtitle: null,
-        icon: android,
-        iconAlt: 'outlines of android phone',
-        selected: false,
-        cost: 1000,
-      },
-    ],
-    active: true,
-  },
-  {
-    id: 3,
-    title: 'Which features do you expect to use?',
-    subtitle: 'Select all that apply.',
-    options: [
-      {
-        id: 1,
-        title: 'Photo/Video',
-        subtitle: null,
-        icon: camera,
-        iconAlt: 'camera outline',
-        selected: false,
-        cost: 250,
-      },
-      {
-        id: 2,
-        title: 'GPS',
-        subtitle: null,
-        icon: gps,
-        iconAlt: 'gps pin',
-        selected: false,
-        cost: 250,
-      },
-      {
-        id: 3,
-        title: 'File Transfer',
-        subtitle: null,
-        icon: upload,
-        iconAlt: 'outline of cloud with arrow pointing up',
-        selected: false,
-        cost: 250,
-      },
-    ],
-    active: false,
-  },
-  {
-    id: 4,
-    title: 'Which features do you expect to use?',
-    subtitle: 'Select all that apply.',
-    options: [
-      {
-        id: 1,
-        title: 'Users/Authentication',
-        subtitle: null,
-        icon: users,
-        iconAlt: 'outline of a person with a plus sign',
-        selected: false,
-        cost: 250,
-      },
-      {
-        id: 2,
-        title: 'Biometrics',
-        subtitle: null,
-        icon: biometrics,
-        iconAlt: 'fingerprint',
-        selected: false,
-        cost: 250,
-      },
-      {
-        id: 3,
-        title: 'Push Notifications',
-        subtitle: null,
-        icon: bell,
-        iconAlt: 'outline of a bell',
-        selected: false,
-        cost: 250,
-      },
-    ],
-    active: false,
-  },
-  {
-    id: 5,
-    title: 'What type of custom features do you expect to need?',
-    subtitle: 'Select one.',
-    options: [
-      {
-        id: 1,
-        title: 'Low Complexity',
-        subtitle: '(Informational)',
-        icon: info,
-        iconAlt: "'i' inside a circle",
-        selected: false,
-        cost: 250,
-      },
-      {
-        id: 2,
-        title: 'Medium Complexity',
-        subtitle: '(Interactive, Customizable, Realtime)',
-        icon: customized,
-        iconAlt: 'two toggle switches',
-        selected: false,
-        cost: 500,
-      },
-      {
-        id: 3,
-        title: 'High Complexity',
-        subtitle: '(Data Modeling and Computation)',
-        icon: data,
-        iconAlt: 'outline of line graph',
-        selected: false,
-        cost: 1000,
-      },
-    ],
-    active: false,
-  },
-  {
-    id: 6,
-    title: 'How many users do you expect?',
-    subtitle: 'Select one.',
-    options: [
-      {
-        id: 1,
-        title: '0-10',
-        subtitle: null,
-        icon: person,
-        iconAlt: 'person outline',
-        selected: false,
-        cost: 1,
-      },
-      {
-        id: 2,
-        title: '10-100',
-        subtitle: null,
-        icon: persons,
-        iconAlt: 'outline of two people',
-        selected: false,
-        cost: 1.125,
-      },
-      {
-        id: 3,
-        title: '100+',
-        subtitle: null,
-        icon: people,
-        iconAlt: 'outline of three people',
-        selected: false,
-        cost: 1.25,
-      },
-    ],
-    active: false,
-  },
-];
-
-const websiteQuestions = [
-  {
-    id: 1,
-    title: 'Which service are you interested in?',
-    active: false,
-    options: [
-      {
-        id: 1,
-        title: 'Custom Software Development',
-        subtitle: null,
-        icon: software,
-        iconAlt: 'three floating screens',
-        selected: false,
-        cost: 0,
-      },
-      {
-        id: 2,
-        title: 'iOS/Android App Development',
-        subtitle: null,
-        icon: mobile,
-        iconAlt: 'outlines of phones and tablets',
-        selected: false,
-        cost: 0,
-      },
-      {
-        id: 3,
-        title: 'Website Development',
-        subtitle: null,
-        icon: website,
-        iconAlt: 'computer outline',
-        selected: false,
-        cost: 0,
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: 'Which type of website are you wanting?',
-    subtitle: 'Select one.',
-    options: [
-      {
-        id: 1,
-        title: 'Basic',
-        subtitle: '(Informational)',
-        icon: info,
-        iconAlt: 'person outline',
-        selected: false,
-        cost: 1000,
-      },
-      {
-        id: 2,
-        title: 'Interactive',
-        subtitle: "(Users, API's, Messaging)",
-        icon: customized,
-        iconAlt: 'outline of two people',
-        selected: false,
-        cost: 2000,
-      },
-      {
-        id: 3,
-        title: 'E-Commerce',
-        subtitle: '(Sales)',
-        icon: globe,
-        iconAlt: 'outline of three people',
-        selected: false,
-        cost: 2500,
-      },
-    ],
-    active: true,
-  },
-];
-
 export default function Estimate(props) {
   const classes = useStyles();
   const theme = useTheme();
@@ -395,6 +60,7 @@ export default function Estimate(props) {
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [message, setMessage] = useState('');
+  const [total, setTotal] = useState(0);
 
   const [emailHelper, setEmailHelper] = useState('');
   const [phoneHelper, setPhoneHelper] = useState('');
@@ -519,6 +185,14 @@ export default function Estimate(props) {
       default:
         break;
     }
+  };
+
+  const getTotal = () => {
+    let cost = 0;
+
+    const selections = questions.map((question) =>
+      question.options.filter((option) => option.selected)
+    );
   };
 
   return (
@@ -650,7 +324,10 @@ export default function Estimate(props) {
           <Button
             variant="contained"
             className={classes.estimateButton}
-            onClick={() => setDialogOpen(true)}
+            onClick={() => {
+              setDialogOpen(true);
+              getTotal();
+            }}
           >
             Get Estimate
           </Button>
